b51fe49a32c369d040d98a54066f7bcb2db59c89
Kevin Klues <klueska@cs.berkeley.edu>
2012-12-13 15:14:53 -0800

    Ported the slab allocator from the Akaros kernel for userspace

--- src/parlib.h
+++ src/parlib.h
@@ -44,17 +44,81 @@
 #include "dtls.h"
 #include "spinlock.h"
 
+#ifndef NULL
+#define NULL ((void*) 0)
+#endif
+
+#ifndef TRUE
+#define TRUE	1
+#endif
+
+#ifndef FALSE
+#define FALSE	0
+#endif
+
+
+/* Constructor to set up the parlib library */
+int parlib_init(struct uthread *main_thread);
+
 enum {
 	PG_RDONLY = 4,
 	PG_RDWR   = 6,
 };
 
-enum {
-	FALSE = 0,
-	TRUE
-};
+#define CHECK_FLAG(flags,bit)   ((flags) & (1 << (bit)))
 
-/* Constructor to set up the parlib library */
-int parlib_init(struct uthread *main_thread);
+#define FOR_CIRC_BUFFER(next, size, var) \
+	for (int _var = 0, var = (next); _var < (size); _var++, var = (var + 1) % (size))
+
+// Rounding operations (efficient when n is a power of 2)
+// Round down to the nearest multiple of n
+#define ROUNDDOWN(a, n)						\
+({								\
+	uintptr_t __a = (uintptr_t) (a);				\
+	(typeof(a)) (__a - __a % (n));				\
+})
+
+// Round up to the nearest multiple of n
+#define ROUNDUP(a, n)						\
+({								\
+	uintptr_t __n = (uintptr_t) (n);				\
+	(typeof(a)) (ROUNDDOWN((uintptr_t) (a) + __n - 1, __n));	\
+})
+
+// Round down to the nearest multiple of n
+#define PTRROUNDDOWN(a, n)						\
+({								\
+	char * __a = (char *) (a);				\
+	(typeof(a)) (__a - (uintptr_t)__a % (n));				\
+})
+// Round pointer up to the nearest multiple of n
+#define PTRROUNDUP(a, n)						\
+({								\
+	uintptr_t __n = (uintptr_t) (n);				\
+	(typeof(a)) (PTRROUNDDOWN((char *) (a) + __n - 1, __n));	\
+})
+
+// Return the integer logarithm of the value provided rounded down
+static inline uintptr_t LOG2_DOWN(uintptr_t value)
+{
+	uintptr_t l = 0;
+	while( (value >> l) > 1 ) ++l;
+	return l;
+}
+
+// Return the integer logarithm of the value provided rounded up
+static inline uintptr_t LOG2_UP(uintptr_t value)
+{
+	uintptr_t _v = LOG2_DOWN(value);
+	if (value ^ (1 << _v))
+		return _v + 1;
+	else
+		return _v;
+}
+
+static inline uintptr_t ROUNDUPPWR2(uintptr_t value)
+{
+	return 1 << LOG2_UP(value);
+}
 
 #endif	// PARLIB_PARLIB_H
