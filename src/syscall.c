d2d85ec4410e035c8501c51761a216ad525d2a87
Kevin Klues <klueska@cs.berkeley.edu>
2014-10-11 12:03:14 -0700

    Only interpose on syscalls if we're a uthread

--- src/syscall.c
+++ src/syscall.c
@@ -17,22 +17,30 @@ int EXPORT_SYMBOL open(const char* path, int oflag, ...)
   int mode = va_arg(vl, int);
   va_end(vl);
 
-  return uthread_blocking_call(__internal_open, path, oflag, mode);
+  if (current_uthread)
+    return uthread_blocking_call(__internal_open, path, oflag, mode);
+  return __internal_open(path, oflag, mode);
 }
 
 int EXPORT_SYMBOL close(int fd)
 {
-  return uthread_blocking_call(__internal_close, fd);
+  if (current_uthread)
+    return uthread_blocking_call(__internal_close, fd);
+  return __internal_close(fd);
 }
 
 ssize_t EXPORT_SYMBOL read(int fd, void* buf, size_t sz)
 {
-  return uthread_blocking_call(__internal_read, fd, buf, sz);
+  if (current_uthread)
+    return uthread_blocking_call(__internal_read, fd, buf, sz);
+  return __internal_read(fd, buf, sz);
 }
 
 ssize_t EXPORT_SYMBOL write(int fd, const void* buf, size_t sz)
 {
-  return uthread_blocking_call(__internal_write, fd, buf, sz);
+  if (current_uthread)
+    return uthread_blocking_call(__internal_write, fd, buf, sz);
+  return __internal_write(fd, buf, sz);
 }
 
 #endif
